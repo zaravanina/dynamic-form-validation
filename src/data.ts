40d951a5f5bb237437fc413681dfdd649bff80ef
Nina <nsz@heyday,dk>
2025-09-24 19:30:37 +0200

    key replacement

--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,9 @@
 const SUPABASE_URL = "https://uyzgkvmxyqwdlyrhvadf.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5emdrdm14eXF3ZGx5cmh2YWRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NjczODIsImV4cCI6MjA3NDA0MzM4Mn0.N_k5pig8glzW0nbhGiyFcJnTjZYW_KW3GL2m2sp4UOg";
+const SUPABASE_KEY = "SPECIAL-KEY"; // replace anon key
 
 // get rows
-export async function getAllContacts() {
-  const res = await fetch(`${SUPABASE_URL}/rest/v1/Contacts?select=*`, {
+export async function getAllFormEntries(): Promise<FormDataEntryValue[]> {
+  const res = await fetch(`${SUPABASE_URL}/rest/v1/formEntries?select=*`, {
     headers: {
       apikey: SUPABASE_KEY,
       Authorization: `Bearer ${SUPABASE_KEY}`,
@@ -14,15 +13,20 @@ export async function getAllContacts() {
 }
 
 // add a row
-export async function addUser(name: string, age: number) {
-  const res = await fetch(`${SUPABASE_URL}/rest/v1/Contacts`, {
-    method: "POST",
-    headers: {
-      apikey: SUPABASE_KEY,
-      Authorization: `Bearer ${SUPABASE_KEY}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name, age }),
-  });
-  return res.json();
+export async function submitFormEntry(body: Record<string, string>) {
+  try {
+    const res = await fetch(`${SUPABASE_URL}/rest/v1/formEntries`, {
+      method: "POST",
+      headers: {
+        apikey: SUPABASE_KEY,
+        Authorization: `Bearer ${SUPABASE_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    return "success";
+  } catch (error) {
+    console.error("Error submitting form entry:", error);
+    throw error;
+  }
 }
